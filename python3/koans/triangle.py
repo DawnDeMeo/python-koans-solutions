2bea673ce2457032965f40073d7e44184663e757
Dawn DeMeo <dawn.demeo@gmail.com>
2020-09-20 16:51:13 -0400

    Completed triangle.py

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,12 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a == b == c:
+    	return 'equilateral'
+    elif a == b or b == c or c == a:
+    	return 'isosceles'
+    else:
+    	return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
